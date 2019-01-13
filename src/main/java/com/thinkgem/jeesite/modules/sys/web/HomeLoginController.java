af6e7d29454dc2dfee6368a4f85bcfca532c931c
git-zsl <328875024@qq.com>
2019-01-14 00:38:32 +0800

    优化注册上传

--- src/main/java/com/thinkgem/jeesite/modules/sys/web/HomeLoginController.java
+++ src/main/java/com/thinkgem/jeesite/modules/sys/web/HomeLoginController.java
@@ -21,6 +21,7 @@ import com.thinkgem.jeesite.modules.sys.utils.EmailUtils;
 import com.thinkgem.jeesite.modules.sys.utils.LogUtils;
 import com.thinkgem.jeesite.modules.sys.utils.LoginUtils;
 import com.thinkgem.jeesite.modules.sys.utils.UserUtils;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
 import org.apache.shiro.cache.Cache;
 import org.apache.shiro.cache.CacheManager;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,6 +32,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+import org.springframework.web.util.WebUtils;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -56,6 +59,7 @@ public class HomeLoginController extends BaseController {
 
     private static final String TRUE = "true";
     private Class clazz = HomeLoginController.class;
+
     /**
      * token验证失败
      */
@@ -63,7 +67,7 @@ public class HomeLoginController extends BaseController {
     @ResponseBody
     public ReturnEntity login(HttpServletRequest request, HttpServletResponse response, Model model) {
         LogUtils.getLogInfo(clazz).info("token无效，请重新登录");
-        return new ReturnEntity(ReturnStatus.UNAUTHORIZED,"token无效，请登录");
+        return new ReturnEntity(ReturnStatus.UNAUTHORIZED, "token无效，请登录");
     }
 
     /**
@@ -72,118 +76,126 @@ public class HomeLoginController extends BaseController {
     @RequestMapping(value = "/homeLogin", method = RequestMethod.GET)
     public String loginFail(@RequestParam Map<String, String> map, boolean isCompany, HttpServletRequest request, User user) {
         String loginName = map.get("loginName");
-        try{
+        try {
             Cache<String, String> cache = cacheManager.getCache(loginName);
             Set<String> keys = cache.keys();
-            if(StringUtils.isBlank(loginName) || keys.size() == 0){
+            if (StringUtils.isBlank(loginName) || keys.size() == 0) {
                 LogUtils.getLogInfo(clazz).info("邮箱验证失败,loginName为空");
                 return "redirect:http://www.baidu.com";
             }
-            for (Iterator<String> it = keys.iterator(); it.hasNext();){
+            for (Iterator<String> it = keys.iterator(); it.hasNext(); ) {
                 String key = it.next();
-                map.put(key,cache.get(key));
+                map.put(key, cache.get(key));
             }
             //开始注册
             homeLoginService.signIn(isCompany, map);
             //清除缓存map
             CacheUtils.removeAll(loginName);
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
             //清除缓存map
             CacheUtils.removeAll(loginName);
-            LogUtils.getLogInfo(clazz).info("邮注册失败",e);
+            LogUtils.getLogInfo(clazz).info("邮注册失败", e);
             return "redirect:http://www.baidu.com";
         }
         return "redirect:http://139.159.200.137:8081/";
     }
-        /**
-         * 邮箱认证
-         */
-        @RequestMapping(value = "/email", method = RequestMethod.POST)
-        @ResponseBody
-        public ReturnEntity<String> sentValidateEmail(HttpServletRequest request,@RequestParam MultipartFile homeImage,@RequestParam MultipartFile file){
-            try{
-                Map<String,String[]> parameterMap = request.getParameterMap();
-                Map<String,String> map = new HashMap<>();
-                for (Map.Entry<String,String[]> entry : parameterMap.entrySet()) {
-                    map.put(entry.getKey(), URLDecoder.decode(entry.getValue()[0],"UTF-8"));
-                }
-                String loginName = map.get("loginName");
-                String originalFilename = homeImage.getOriginalFilename();
+
+    /**
+     * 邮箱认证
+     */
+    @RequestMapping(value = "/email", method = RequestMethod.POST)
+    @ResponseBody
+    public ReturnEntity<String> sentValidateEmail(HttpServletRequest request) {
+        try {
+            Map<String, String[]> parameterMap = request.getParameterMap();
+            Map<String, String> map = new HashMap<>();
+            for (Map.Entry<String, String[]> entry : parameterMap.entrySet()) {
+                map.put(entry.getKey(), URLDecoder.decode(entry.getValue()[0], "UTF-8"));
+            }
+            MultipartFile file = null;
+            MultipartFile image = null;
+            String loginName = map.get("loginName");
+            boolean isMultipart = ServletFileUpload.isMultipartContent(request);
+            if (isMultipart) {
+                MultipartHttpServletRequest multipartRequest = WebUtils.getNativeRequest(request, MultipartHttpServletRequest.class);
+                file = multipartRequest.getFile("file");
+                image = multipartRequest.getFile("image");
+                String originalFilename = image.getOriginalFilename();
                 String originalFilename1 = file.getOriginalFilename();
-                String configPath = Global.getConfig("userfiles.basedir").substring(0,1) + Global.getConfig("userfiles.basedir").substring(1);
-                File file1 = new File(configPath + "/" + loginName +"/" + originalFilename);
-                File file2 = new File(configPath + "/" + loginName +"/" + originalFilename1);
-                if(!file1.getParentFile().exists()){
+                String configPath = Global.getConfig("userfiles.basedir").substring(0, 1) + Global.getConfig("userfiles.basedir").substring(1);
+                File file1 = new File(configPath + "/" + loginName + "/" + originalFilename);
+                File file2 = new File(configPath + "/" + loginName + "/" + originalFilename1);
+                if (!file1.getParentFile().exists()) {
                     file1.getParentFile().mkdirs();
                 }
-                homeImage.transferTo(file1);
+                image.transferTo(file1);
                 file.transferTo(file2);
-                map.put("image",file1.getPath());
-                map.put("file",file2.getPath());
-                //个人用户字段
-                String email = map.get("email");
-                email = URLDecoder.decode(email);
-                String isCompany = map.get("isCompany");
-                //用户名验证
-                if(StringUtils.isBlank(loginName)){
-                    LogUtils.getLogInfo(clazz).info("用户名不能为空");
-                    return ReturnEntity.fail("用户名不能为空");
-                }
-                User byLoginName = UserUtils.getByLoginName(loginName);
-                if(!Objects.isNull(byLoginName)){
-                    return ReturnEntity.fail("当前用户名已被占用");
-                }
-                //企业用户字段
-
-                //清除缓存map
-                    CacheUtils.removeAll(loginName);
-                if(TRUE.equals(isCompany)){
-                    map.put("isCompany","true");
-                    String companyId = UserUtils.getCompanyId(Global.OFFICE_TYPE_2);
-                    map.put("companyId",companyId);
-                    //需要增加部门（部门字段未添加）
-
-                }else{
-                    String officeId = UserUtils.getOfficeId(Global.OFFICE_TYPE_1);
-                    String companyId = UserUtils.getCompanyId(Global.OFFICE_TYPE_1);
-                    map.put("officeId",officeId);
-                    map.put("companyId",companyId);
-                    map.put("isCompany","false");
-                    map.put("name",loginName);
-                }
-                if (StringUtils.isBlank(map.get("loginName")) && StringUtils.isBlank(email)) {
-                    return ReturnEntity.fail("用户名或者邮箱不能为空");
-                }
-                    String content = "<a href=http://"+ Global.getConfig("serverAddress")+"/homeLogin?loginName="+loginName+"&isCompany="+isCompany+">请点击完成此处激活帐号完成注册</a><br/>";
-                    EmailUtils.sendHtmlMail(new Email(email,"注册验证",content));
-                    CacheUtils.putMapAll(loginName,map);
-            }catch (Exception e){
-                LogUtils.getLogInfo(clazz).info("邮箱认证出错",e);
-                e.printStackTrace();
-                return ReturnEntity.fail("系统出错，请联系管理员");
+                map.put("image", file1.getPath());
+                map.put("file", file2.getPath());
             }
-            return ReturnEntity.success("邮件发送成功");
+            //个人用户字段
+            String email = map.get("email");
+            email = URLDecoder.decode(email);
+            String isCompany = map.get("isCompany");
+            //用户名验证
+            if (StringUtils.isBlank(loginName)) {
+                LogUtils.getLogInfo(clazz).info("用户名不能为空");
+                return ReturnEntity.fail("用户名不能为空");
+            }
+            User byLoginName = UserUtils.getByLoginName(loginName);
+            if (!Objects.isNull(byLoginName)) {
+                return ReturnEntity.fail("当前用户名已被占用");
+            }
+            //企业用户字段
+            //清除缓存map
+            CacheUtils.removeAll(loginName);
+            if (TRUE.equals(isCompany)) {
+                map.put("isCompany", "true");
+                String companyId = UserUtils.getCompanyId(Global.OFFICE_TYPE_2);
+                map.put("companyId", companyId);
+                //需要增加部门（部门字段未添加）
+
+            } else {
+                String officeId = UserUtils.getOfficeId(Global.OFFICE_TYPE_1);
+                String companyId = UserUtils.getCompanyId(Global.OFFICE_TYPE_1);
+                map.put("officeId", officeId);
+                map.put("companyId", companyId);
+                map.put("isCompany", "false");
+                map.put("name", loginName);
+            }
+            if (StringUtils.isBlank(map.get("loginName")) && StringUtils.isBlank(email)) {
+                return ReturnEntity.fail("用户名或者邮箱不能为空");
+            }
+            String content = "<a href=http://" + Global.getConfig("serverAddress") + "/homeLogin?loginName=" + loginName + "&isCompany=" + isCompany + ">请点击完成此处激活帐号完成注册</a><br/>";
+            EmailUtils.sendHtmlMail(new Email(email, "注册验证", content));
+            CacheUtils.putMapAll(loginName, map);
+        } catch (Exception e) {
+            LogUtils.getLogInfo(clazz).info("邮箱认证出错", e);
+            e.printStackTrace();
+            return ReturnEntity.fail("系统出错，请联系管理员");
         }
+        return ReturnEntity.success("邮件发送成功");
+    }
 
     /**
      * 用户名验证
      */
     @RequestMapping(value = "/ValidateLoginName", method = RequestMethod.POST)
     @ResponseBody
-    public ReturnEntity<String> ValidateLoginName(@RequestParam Map<String,String> map){
-        try{
+    public ReturnEntity<String> ValidateLoginName(@RequestParam Map<String, String> map) {
+        try {
             String loginName = map.get("loginName");
-            if(StringUtils.isBlank(loginName)){
+            if (StringUtils.isBlank(loginName)) {
                 LogUtils.getLogInfo(clazz).info("用户名不能为空");
-               return ReturnEntity.fail("用户名不能为空");
+                return ReturnEntity.fail("用户名不能为空");
             }
             User byLoginName = UserUtils.getByLoginName(loginName);
-            if(Objects.isNull(byLoginName)){
+            if (Objects.isNull(byLoginName)) {
                 return ReturnEntity.success("当前用户名可以使用");
             }
-        }catch (Exception e){
-            LogUtils.getLogInfo(clazz).info("查询出错",e);
+        } catch (Exception e) {
+            LogUtils.getLogInfo(clazz).info("查询出错", e);
             e.printStackTrace();
             return ReturnEntity.fail("系统内部出错");
         }
@@ -195,36 +207,36 @@ public class HomeLoginController extends BaseController {
      */
     @RequestMapping(value = "/loginSuccess", method = RequestMethod.POST)
     @ResponseBody
-    public ReturnEntity<String> loginSuccess(@RequestParam Map<String,String> map,@RequestParam(value = "isComapny",required = false) String isCompany){
-        try{
+    public ReturnEntity<String> loginSuccess(@RequestParam Map<String, String> map, @RequestParam(value = "isComapny", required = false) String isCompany) {
+        try {
             String token = "";
             String loginName = map.get("loginName");
             String password = map.get("password");
-            if(StringUtils.isBlank(loginName)){
+            if (StringUtils.isBlank(loginName)) {
                 return ReturnEntity.fail("用户名不能为空");
             }
-            if(StringUtils.isBlank(password)){
+            if (StringUtils.isBlank(password)) {
                 return ReturnEntity.fail("密码不能为空");
             }
             User byLoginName = UserUtils.getByLoginName(loginName);
-            if(Objects.isNull(byLoginName)){
+            if (Objects.isNull(byLoginName)) {
                 return ReturnEntity.fail("用户名错误，请重新输入");
             }
-            if(!byLoginName.getIsCompany().equals(isCompany)){
-                return ReturnEntity.fail(Global.TRUE.equals(isCompany)?"请选择个人登录入口登录":"请选择企业登录入口登录");
+            if (!byLoginName.getIsCompany().equals(isCompany)) {
+                return ReturnEntity.fail(Global.TRUE.equals(isCompany) ? "请选择个人登录入口登录" : "请选择企业登录入口登录");
             }
             //验证密码
-            if(LoginUtils.validatePassword(password,byLoginName.getPassword())){
+            if (LoginUtils.validatePassword(password, byLoginName.getPassword())) {
                 //生成token
                /* String format = MessageFormat.format("{}{}", loginName, Global.ZSL);*/
                 String format = loginName + Global.ZSL;
                 token = LoginUtils.entryptPassword(format);
                 byLoginName.setToken(token);
                 byLoginName.setIsCompany(isCompany);
-                return ReturnEntity.success(byLoginName,"登录成功");
+                return ReturnEntity.success(byLoginName, "登录成功");
             }
-        }catch (Exception e){
-            LogUtils.getLogInfo(clazz).info("程序出错",e);
+        } catch (Exception e) {
+            LogUtils.getLogInfo(clazz).info("程序出错", e);
             return ReturnEntity.fail("程序出错");
         }
         return ReturnEntity.success("登录成功");
@@ -235,22 +247,22 @@ public class HomeLoginController extends BaseController {
      */
     @RequestMapping("/getArea")
     @ResponseBody
-    public ReturnEntity<Area> getAreaData(){
+    public ReturnEntity<Area> getAreaData() {
         List<Area> areas = Lists.newArrayList();
-        try{
+        try {
             List<Area> list = areaService.findTopArea();
             for (Area area : list) {
-                if(Global.YES.equals(area.getParent().getId())){
+                if (Global.YES.equals(area.getParent().getId())) {
                     areas.add(area);
                     areaService.findChildArea(area);
                 }
             }
-        }catch (Exception e){
-            LogUtils.getLogInfo(clazz).info("获取省市区失败",e);
+        } catch (Exception e) {
+            LogUtils.getLogInfo(clazz).info("获取省市区失败", e);
             e.printStackTrace();
             return ReturnEntity.fail("获取省市区失败");
         }
-        return ReturnEntity.success(areas,"获取省市区成功");
+        return ReturnEntity.success(areas, "获取省市区成功");
     }
 
     /**
@@ -258,9 +270,9 @@ public class HomeLoginController extends BaseController {
      */
     @RequestMapping(value = "upload")
     @ResponseBody
-    public ReturnEntity upload(@RequestParam MultipartFile file)throws Exception{
+    public ReturnEntity upload(@RequestParam MultipartFile file) throws Exception {
         String originalFilename = file.getOriginalFilename();
-        File file1 = new File("F://11/"+originalFilename);
+        File file1 = new File("F://11/" + originalFilename);
         file.transferTo(file1);
         return ReturnEntity.success("上传成功");
     }
